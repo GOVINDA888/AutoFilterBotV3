129af4481f99201cd4177d86d403ab830ac9a43f
GOVINDA888 <105217504+GOVINDA888@users.noreply.github.com>
2022-08-16 08:00:59 +0530

    Update Commands.py

--- LuciferMoringstar_Robot/Commands.py
+++ LuciferMoringstar_Robot/Commands.py
@@ -84,7 +84,7 @@ async def start(bot, message):
                     f_caption = f"{files.file_name}"
                 buttons = [
                     [
-                        InlineKeyboardButton('🎖 DEPLOY YOURS 🎖', url=f'{TUTORIAL}')
+                        InlineKeyboardButton('🎖 Tamil New Movies 🎖', url=f'{https://t.me/+hSvUkQYM7BVjMmM1}')
                     ]
                     ]
                 await bot.send_cached_media(
